9b9378c2280b60c89cff46013bd8ad060644d742
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-02-09 22:28:54 -0600

    app

--- app.js
+++ app.js
@@ -2,50 +2,42 @@
 const express = require("express");
 const app = express();
 const PORT = 3000 || process.env.PORT;
+const session = require("express-session");
 
-// Configuración de public //
+// Configuración de public, src para todo el proyecto
 app.use(express.static("public"));
-app.use(express.static("assets"));
+// app.use(express.static("assets"));
+// app.use(express.static("src"));
+app.use(express.urlencoded({ extended: false }));
 
 //Rutas
 app.set("view engine", "ejs");
 app.set("views", "./views/pages");
+app.use(
+  session({
+    secret: "shh, its a secrete",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+const mainRoute = require("./src/routes/mainRoute");
+const userRoute = require("./src/routes/userRoute");
+const productRoute = require("./src/routes/productRoute");
+const storeRoute = require("./src/routes/storeRoute");
+const cartRoute = require("./src/routes/cartRoute");
 
 // INDEX
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-// PRODUCTS
-app.get("/products", (req, res) => {
-  res.render("product");
-});
-// PRODUCT/:ID
-app.get("/products/:productID", (req, res) => {
-  let productID = req.params.productID;
-
-  res.render("product_error.ejs", { productID });
-});
-
-// CART
-app.get("/cart", (req, res) => {
-  res.render("cart");
-});
+app.use("/", mainRoute);
+app.use("/users", userRoute);
+app.use("/", productRoute);
+app.use("/", storeRoute);
+app.use("/", cartRoute);
 
 // CHECKOUT
-app.get("/checkout", (req, res) => {
-  res.render("checkout");
+app.get("*", function (req, res) {
+  res.status(404).render("checkout");
 });
 
-// REGISTER
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-// LOGIN
-app.get("/login", (req, res) => {
-  res.render("login");
-});
 //Servidor
 app.listen(PORT, () => {
   console.log("SERVIDOR CORRIENDO EN EL PUERTO 3000");
